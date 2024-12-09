5572bfafcc5568a648da653e47387e89f398928c
Rabbi1089 <rabbi.ext@gmail.com>
2024-12-09 18:39:23 +0600

    add Trending Articles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,10 @@
+import Slider from "../../components/Slider/Slider";
+
 const Home = () => {
     return (
         <div>
             <h1 className=" text-6xl">I am from home</h1>
+            <Slider />
         </div>
     );
 };
