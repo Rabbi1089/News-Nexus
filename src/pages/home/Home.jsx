e82bfe8318e5815f4aff1f0b14db0045a122a8ae
Rabbi1089 <rabbi.ext@gmail.com>
2024-12-09 18:54:39 +0600

    add Publisher, Statistic ,Plans sections

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
+import Plans from "../../components/Plans/Plans";
+import Publisher from "../../components/Publisher/Publisher";
 import Slider from "../../components/Slider/Slider";
+import Statistic from "../../components/Statistic/Statistic";
 
 const Home = () => {
     return (
         <div>
             <h1 className=" text-6xl">I am from home</h1>
             <Slider />
+            <Publisher />
+            <Statistic />
+            <Plans />
         </div>
     );
 };
